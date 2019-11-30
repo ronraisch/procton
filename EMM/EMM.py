fixing bug for calculation of black move

EMM/EMM.py
@@ -3,40 +3,68 @@
 from GUI import *
 
 
-def p(state):
+def get_prob(state):
     if state[GameState.DICE_RESULT] == state[GameState.DICE_RESULT]:
         return 1 / 36
     return 1 / 18
 
 
-def EMM(state_list, depth, alpha, beta, player_turn):
-    state = GameState(state_list)
-    if depth == 0 or state.game_over():
-        return state.evaluate(),state_list
+counter = 0
 
+
+def EMM(state_list, depth, player_turn):
+    state = GameState(state_list)
     max_val = -np.inf
     max_state = None
     for board in state.get_possible_states(player_turn):
-        for i in range(GameState.MIN_DICE_VALUE,GameState.MAX_DICE_VALUE):
-            for j in range(i,GameState.MAX_DICE_VALUE):
-                new_board=board.copy()
-                new_board[GameState.DICE_RESULT:GameState.DICE_RESULT+2]=np.array([i,j])
-                tmp = GameState(new_board)
-                val, dummie = EMM(tmp.state, depth - 1, alpha, beta, -player_turn) * p(tmp.state)
+        result = EMM_wo_dices(board, depth - 1, -np.inf, -np.inf, -player_turn)
+        if result[0] > max_val:
+            max_val = result[0]
+            max_state = board
+    return max_val, max_state
+
+
+def EMM_wo_dices(state_list, depth, alpha, beta, player_turn):
+    # global counter
+    # counter += 1
+    # print(counter)
+    state = GameState(state_list)
+    if depth == 0 or state.game_over():
+        return state.evaluate(), state_list
+
+    max_val = -np.inf
+    max_state = None
+    for i in range(GameState.MIN_DICE_VALUE, GameState.MAX_DICE_VALUE+1):
+        for j in range(i, GameState.MAX_DICE_VALUE+1):
+            new_board = state_list.copy()
+            new_board[GameState.DICE_RESULT:GameState.DICE_RESULT + 2] = np.array([i, j])
+            state_dice = GameState(new_board)
+            # GUI_state(state_dice.state)
+            print(state_dice.state,player_turn)
+            for board in state_dice.get_possible_states(player_turn):
+                print("Sdf")
+                tmp = GameState(board)
+                val, dummie = EMM_wo_dices(tmp.state, depth - 1, alpha, beta, -player_turn)
+                val *= get_prob(tmp.state)
+
                 if max_val < val:
                     max_val = val
                     max_state = tmp.state.copy()
                 alpha = max(alpha, val)
-                if beta <= alpha:
-                    print(max_val,max_state,"hj")
-                    return max_val, max_state
-    print(max_val, max_state)
+                # if beta <= alpha:
+                #     print(max_val, max_state, "alpha beta")
+                #     return max_val, max_state
+    # print(max_val, max_state)
     return max_val, max_state
 
 
-result=EMM(INITIAL_STATE,0,-np.inf,-np.inf,1)
-print(result[0])
+
+result = EMM(INITIAL_STATE, 2, 1)
+print(result)
 GUI_state(result[1])
+
+my_state = GameState(np.array(result[1]))
+print(my_state.evaluate())
 
 # if depth == 0 or state.game_over():
 #     return evaluate(state)

EMM/GUI.py
@@ -5,7 +5,7 @@ import matplotlib.patches as patches
 # white starts at the bottom left
 INITIAL_STATE = np.array(
     [0, 2.0, 0, 0, 0, 0, -5.0, 0, -3.0, 0, 0, 0, 5.0, -5.0, 0, 0, 0, 3.0, 0, 5.0, 0, 0, 0, 0, -2.0,
-     0, 0, 0, 6, 6])
+     0, 0, 0, 6, 1])
 test_state = np.array(
     [0, 0, 0, 0, 0, 0, -6.0, 0, -3.0, 0, 0, 0, 0, -5.0, 0, 0, 0, 0, 0, 5.0, 5.0, 5.0, 0, 0, -1.0,
      0, 0, 0, 5, 3])
@@ -22,7 +22,7 @@ def GUI_state(l):
     for pos in x[1:]:
         plt.plot(np.ones_like(x) * pos - 1.0 / NUM_OF_POSITIONS,
                  (x - 1.0 / 2 / NUM_OF_POSITIONS) * 1.05, 'r')
-    print(l)
+    # print(l)
     x_rect = 1 + 1 / SOLDIERS_IN_ROW
     y_rect = 0.2
     plt.text(x_rect, y_rect / 2,

EMM/game_state.py
@@ -7,6 +7,7 @@ class GameState:
     # the positive rotation in the board is in the white's direction
     # the first index of eating places are for whites
     # move="out" => removing soldier from game
+
     INITIAL_SOLDIER = 0
     NUMBER_OF_POSITIONS = 26 + INITIAL_SOLDIER
     NUMBER_OF_SOLDIERS = 15
@@ -15,6 +16,9 @@ class GameState:
     MAX_DICE_VALUE = 6
     KILLED_SOLDIERS_INDEX = NUMBER_OF_POSITIONS + INITIAL_SOLDIER
     DICE_RESULT = KILLED_SOLDIERS_INDEX + 2
+    score1 = np.power(np.arange(-NUMBER_OF_POSITIONS / 2 + 1, 0), 3)
+    score2 = np.power(np.arange(1, NUMBER_OF_POSITIONS / 2), 3)
+    SCORE = np.concatenate((score1, -score2))
 
     def __init__(self, l):
         self.state = l
@@ -63,14 +67,15 @@ class GameState:
         possible_moves = []
         dice_result = self.state[GameState.DICE_RESULT:GameState.DICE_RESULT + 2]
         for i in range(GameState.NUMBER_OF_POSITIONS):
-            if GameState.is_legal_move(self.state, [i, i + dice_result[0]], player_turn):
-                move1 = [i, i + dice_result[0]]
+            move1 = [i, i + dice_result[0] * player_turn]
+            if GameState.is_legal_move(self.state, move1, player_turn):
                 eval_state = GameState.make_move_on_state([move1], self.state, player_turn)
             else:
                 continue
             for j in range(GameState.NUMBER_OF_POSITIONS):
-                if GameState.is_legal_move(eval_state, [j, j + dice_result[1]], player_turn):
-                    possible_moves.append([move1, [j, j + dice_result[1]]])
+                move2=[j, j + dice_result[1]*player_turn]
+                if GameState.is_legal_move(eval_state, move2, player_turn):
+                    possible_moves.append([move1, move2])
         return possible_moves
 
     @staticmethod
@@ -110,13 +115,14 @@ class GameState:
         # if move[0] is in the same color of the player.
         if state[move[0]] * player_turn <= 0:
             return False
+
+
         dice_result = state[GameState.DICE_RESULT:GameState.DICE_RESULT + 2]
 
         # if move corresponds to dice results
         # TODO: check if condition necessary (for optimization)
         if (move[1] - move[0]) * player_turn not in dice_result:
             return False
-
         # if move in the board
         if move[1] >= GameState.NUMBER_OF_POSITIONS:
             return False
@@ -145,9 +151,15 @@ class GameState:
                 tmp_state[GameState.KILLED_SOLDIERS_INDEX + (player_turn + 1) / 2] += 1
         return tmp_state
 
+    def get_open_houses(self):
+        total = 0
+        for pos in self.state[1:GameState.NUMBER_OF_POSITIONS - 1]:
+            if np.abs(pos) == 1:
+                total += pos
+        return total
+
     def evaluate(self):
-        score = np.arange(-GameState.NUMBER_OF_POSITIONS / 2 + 1, GameState.NUMBER_OF_POSITIONS / 2)
-        return np.dot(score, self.state[1:GameState.NUMBER_OF_POSITIONS])
+        return np.dot(GameState.SCORE, self.state[1:GameState.NUMBER_OF_POSITIONS - 1])+GameState.SCORE[0]*self.get_open_houses()
 
 # # add eating and removing soldiers
 #         possible_moves = []